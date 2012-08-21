4597ff64c0f11c3da7d0d2ae8345bdc370e4daa7
Damon Haley <damon.haley@colorado.edu>
2012-08-21 10:00:47 -0600

    Tue Aug 21 10:00:46 MDT 2012

--- init.el
+++ init.el
@@ -608,6 +608,31 @@
       (unfill-paragraph 1)
       (forward-paragraph))))
 
+
+;;;_, Toggle between split windows and a single window
+
+(defun my-iswitchb-close()
+"Open iswitchb or, if in minibuffer go to next match. Handy way to cycle through the ring."
+(interactive)
+(if (window-minibuffer-p (selected-window))
+(keyboard-escape-quit)))
+
+(defun toggle-windows-split()
+  "Switch back and forth between one window and whatever split of windows we might have in the frame. The idea is to maximize the current buffer, while being able to go back to the previous split of windows in the frame simply by calling this command again."
+  (interactive)
+  (if (not (window-minibuffer-p (selected-window)))
+      (progn
+        (if (< 1 (count-windows))
+            (progn
+              (window-configuration-to-register ?u)
+              (delete-other-windows))
+          (jump-to-register ?u))))
+  (my-iswitchb-close))
+
+(define-key global-map (kbd "C-|") 'toggle-windows-split)
+
+
+
 ;;;_ , ctl-period-map
 
 ;;;_  . C-. ?
@@ -2419,6 +2444,14 @@ FORM => (eval FORM)."
 (use-package log4j-mode
   :disabled t
   :mode ("\\.log\\'" . log4j-mode))
+
+
+;;;_ , lorem-ipsum
+(use-package lorem-ipsum
+  :commands (Lorem-ipsum-insert-paragraphs
+             Lorem-ipsum-insert-sentences
+             Lorem-ipsum-insert-list)
+  )
 
 ;;;_ , lua-mode
 
