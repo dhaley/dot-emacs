64d176c6d52dd2288250a71c47acc99fef3c27bb
Damon Haley <damon.haley@colorado.edu>
2012-09-10 13:00:46 -0600

    Mon Sep 10 13:00:46 MDT 2012

--- lisp/dkh-powerline.el
+++ lisp/dkh-powerline.el
@@ -6,22 +6,22 @@
 static char * arrow_right[] = {
 \"10 15 2 1\",
 \". c %s\",
-\" c %s\",
-\". \",
-\".. \",
-\"... \",
-\".... \",
-\"..... \",
-\"...... \",
-\"....... \",
-\"........ \",
-\"....... \",
-\"...... \",
-\"..... \",
-\".... \",
-\"... \",
-\".. \",
-\". \"};" color1 color2))
+\"  c %s\",
+\".         \",
+\"..        \",
+\"...       \",
+\"....      \",
+\".....     \",
+\"......    \",
+\".......   \",
+\"........  \",
+\".......   \",
+\"......    \",
+\".....     \",
+\"....      \",
+\"...       \",
+\"..        \",
+\".         \"};"  color1 color2))
 
 (defun arrow-left-xpm (color1 color2)
   "Return an XPM right arrow string representing."
@@ -29,22 +29,22 @@ static char * arrow_right[] = {
 static char * arrow_right[] = {
 \"10 15 2 1\",
 \". c %s\",
-\" c %s\",
-\" .\",
-\" ..\",
-\" ...\",
-\" ....\",
-\" .....\",
-\" ......\",
-\" .......\",
-\" ........\",
-\" .......\",
-\" ......\",
-\" .....\",
-\" ....\",
-\" ...\",
-\" ..\",
-\" .\"};" color2 color1))
+\"  c %s\",
+\"         .\",
+\"        ..\",
+\"       ...\",
+\"      ....\",
+\"     .....\",
+\"    ......\",
+\"   .......\",
+\"  ........\",
+\"   .......\",
+\"    ......\",
+\"     .....\",
+\"      ....\",
+\"       ...\",
+\"        ..\",
+\"         .\"};"  color2 color1))
 
 
 (defconst color1 "#859900")
@@ -53,31 +53,31 @@ static char * arrow_right[] = {
 (defconst fontcolor1 "#FDF6E3")
 (defconst fontcolor2 "#EEE8D5")
 
-(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2)
+(defvar arrow-right-1 (create-image (arrow-right-xpm color1 color2) 
                                     'xpm t :ascent 'center))
-(defvar arrow-right-2 (create-image (arrow-right-xpm color2 "None")
+(defvar arrow-right-2 (create-image (arrow-right-xpm color2 "None") 
                                     'xpm t :ascent 'center))
-(defvar arrow-left-1 (create-image (arrow-left-xpm color2 color3)
+(defvar arrow-left-1  (create-image (arrow-left-xpm color2 color3) 
                                     'xpm t :ascent 'center))
-(defvar arrow-left-2 (create-image (arrow-left-xpm "None" color2)
+(defvar arrow-left-2  (create-image (arrow-left-xpm "None" color2) 
                                     'xpm t :ascent 'center))
 
 (setq-default mode-line-format
- (list '(:eval (concat (propertize " %m " 'face 'mode-line-color-1)
+ (list  '(:eval (concat (propertize " %m " 'face 'mode-line-color-1)
                         (propertize " " 'display arrow-right-1)))
         '(:eval (concat (propertize " %* %b " 'face 'mode-line-color-2)
                         (propertize " " 'display arrow-right-2)))
 
         ;; Justify right by filling with spaces to right fringe - 16
         ;; (16 should be computed rahter than hardcoded)
-        '(:eval (propertize " "
-                            'display
+        '(:eval (propertize " " 
+                            'display 
                             '((space :align-to (- right-fringe 17)))))
 
         '(:eval (concat (propertize " " 'display arrow-left-2)
                         (propertize " %p " 'face 'mode-line-color-2)))
         '(:eval (concat (propertize " " 'display arrow-left-1)
-                        (propertize "%4l:%2c " 'face 'mode-line-color-3)))
+                        (propertize "%4l:%2c  " 'face 'mode-line-color-3)))
 ))
 
 (make-face 'mode-line-color-1)
