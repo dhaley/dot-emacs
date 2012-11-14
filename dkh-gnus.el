598408a8aa6ba041f64e52715d2e1f9aacc427c0
Damon Haley <damon.haley@colorado.edu>
2012-11-14 13:00:39 -0700

    Wed Nov 14 13:00:39 MST 2012

--- dkh-gnus.el
+++ dkh-gnus.el
@@ -383,11 +383,12 @@ is:
     (defun gnus-goto-article (message-id)
       (activate-gnus)
       (gnus-summary-read-group "INBOX" 15 t)
-      (let ((nnir-imap-default-search-key "imap")
-            (nnir-ignored-newsgroups
-             (concat "\\(\\(list\\.wg21\\|archive\\)\\.\\|"
-                     "mail\\.\\(spam\\|save\\|trash\\|sent\\)\\)")))
-        (gnus-summary-refer-article message-id)))
+      (gnus-summary-refer-article message-id))
+      ;; (let ((nnir-imap-default-search-key "imap")
+      ;;       (nnir-ignored-newsgroups
+      ;;        (concat "\\(\\(list\\.wg21\\|archive\\)\\.\\|"
+      ;;                "mail\\.\\(spam\\|save\\|trash\\|sent\\)\\)")))
+      ;;   (gnus-summary-refer-article message-id)))
 
     (defvar gnus-query-history nil)
 
@@ -405,21 +406,23 @@ is:
             (nnir-ignored-newsgroups
              (if arg
                  (concat (regexp-opt
-                          '("archive"
-                            "archive.emacs"
-                            "list"
-                            "list.bahai"
-                            "list.boost"
-                            "list.clang"
+                          '(
+                            ;; "archive"
+                            ;; "archive.emacs"
+                            ;; "list"
+                            ;; "list.bahai"
+                            ;; "list.boost"
+                            ;; "list.clang"
                             "list.emacs"
-                            "list.isocpp"
-                            "list.ledger"
-                            "list.llvm"
-                            "list.wg21"
-                            "mail"
-                            "mail.save"
-                            "Drafts"
-                            "Sent Messages"))
+                            ;; "list.isocpp"
+                            ;; "list.ledger"
+                            ;; "list.llvm"
+                            ;; "list.wg21"
+                            ;; "mail"
+                            ;; "mail.save"
+                            ;; "Drafts"
+                            "Sent Messages"
+                            ))
                          "\\'")
                (concat "\\(\\(list\\|archive\\)\\.\\|"
                        "mail\\.\\(spam\\|save\\|trash\\|sent\\)\\)"))))
