d601bda6ef275dc0a01a4b82b9946db21537e66e
Damon Haley <damon.haley@colorado.edu>
2012-11-25 20:01:02 -0700

    Sun Nov 25 20:01:02 MST 2012

--- init.el
+++ init.el
@@ -893,6 +893,20 @@ Subexpression references can be used (\1, \2, etc)."
         (insert (make-string count ?\*))))))
 
 
+(defun clean-up-buffer-or-region ()
+  "Untabifies, indents and deletes trailing whitespace from buffer or region."
+  (interactive)
+  (save-excursion
+    (unless (region-active-p)
+      (mark-whole-buffer))
+    (untabify (region-beginning) (region-end))
+    (indent-region (region-beginning) (region-end))
+    (save-restriction
+      (narrow-to-region (region-beginning) (region-end))
+      (delete-trailing-whitespace))))
+
+(global-set-key (kbd "H-n") 'clean-up-buffer-or-region)
+
 
 
 
