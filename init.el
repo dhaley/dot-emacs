ec0c45b7d0b73c0044da9ffe922e30a51d8efce2
Damon Haley <damon.haley@colorado.edu>
2012-12-31 15:35:04 -0700

    now works with emacs pre-release 24.2.91.1.

--- init.el
+++ init.el
@@ -1265,10 +1265,10 @@ Subexpression references can be used (\1, \2, etc)."
 
 ;;;_ , ansi-term
 
-;; (use-package ansi-term
-;;   :defer t
-;;   :init
-;;   (progn
+(use-package ansi-term
+  :defer t
+  :init
+  (progn
 
 ;;     ;; ;; Use variable width font faces in current buffer
 ;;     (defun my-buffer-face-mode-variable ()
@@ -1278,47 +1278,47 @@ Subexpression references can be used (\1, \2, etc)."
 ;;       (text-scale-adjust 1)
 ;;       (buffer-face-mode))
 
-;;     (setq system-uses-terminfo nil)
-;;     ;; When you use this code, note that dabbrev-completion is C-x /, and yanking is C-y.
+    (setq system-uses-terminfo nil)
+    ;; When you use this code, note that dabbrev-completion is C-x /, and yanking is C-y.
 
-;;     (add-hook 'term-mode-hook
-;;               '(lambda ()
-;;                  (linum-mode 0)
-;;                  (term-set-escape-char ?\C-z)
-;;                  (term-set-escape-char ?\C-x)
-;;                  (define-key term-raw-map "\C-c" 'term-interrupt-subjob)
-;;                  (define-key term-raw-map (kbd "M-x") 'execute-extended-command)
-;;                  (define-key term-raw-escape-map "/"
-;;                    (lambda ()
-;;                      (interactive)
-;;                      (let ((beg (point)))
-;;                        (dabbrev-expand nil)
-;;                        (kill-region beg (point)))
-;;                      (term-send-raw-string (substring-no-properties (current-kill 0)))))
-;;                  (setq autopair-dont-activate t)
-;;                  (setq ac-auto-start nil)
-;;                  (visual-line-mode -1)
-;;                  ;; (my-buffer-face-mode-variable)
-;;                  ))
+    (add-hook 'term-mode-hook
+              '(lambda ()
+                 (linum-mode 0)
+                 (term-set-escape-char ?\C-z)
+                 (term-set-escape-char ?\C-x)
+                 (define-key term-raw-map "\C-c" 'term-interrupt-subjob)
+                 (define-key term-raw-map (kbd "M-x") 'execute-extended-command)
+                 (define-key term-raw-escape-map "/"
+                   (lambda ()
+                     (interactive)
+                     (let ((beg (point)))
+                       (dabbrev-expand nil)
+                       (kill-region beg (point)))
+                     (term-send-raw-string (substring-no-properties (current-kill 0)))))
+                 (setq autopair-dont-activate t)
+                 (setq ac-auto-start nil)
+                 (visual-line-mode -1)
+                 ;; (my-buffer-face-mode-variable)
+                 ))
 
 
-;;     (defun my-term-paste (&optional string)
-;;       (interactive)
-;;       (process-send-string
-;;        (get-buffer-process (current-buffer))
-;;        (if string string (current-kill 0))))
+    (defun my-term-paste (&optional string)
+      (interactive)
+      (process-send-string
+       (get-buffer-process (current-buffer))
+       (if string string (current-kill 0))))
 
-;;     (defun my-term-pasteboard-paste ()
-;;       (interactive)
-;;       (process-send-string
-;;        (get-buffer-process (current-buffer))
-;;        (ns-get-pasteboard)))
+    (defun my-term-pasteboard-paste ()
+      (interactive)
+      (process-send-string
+       (get-buffer-process (current-buffer))
+       (ns-get-pasteboard)))
 
-;;     (add-hook 'term-exec-hook '(lambda ()
-;;                                  (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)
-;;                                  (goto-address-mode)
-;;                                  (define-key term-raw-map (kbd "C-y") 'my-term-paste)
-;;                                  (define-key term-raw-map (kbd "s-v") 'my-term-pasteboard-paste)
+    (add-hook 'term-exec-hook '(lambda ()
+                                 (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix)
+                                 (goto-address-mode)
+                                 (define-key term-raw-map (kbd "C-y") 'my-term-paste)
+                                 (define-key term-raw-map (kbd "s-v") 'my-term-pasteboard-paste)
 ;;                                  (let ((base03 "#002b36")
 ;;                                        (base02 "#073642")
 ;;                                        (base01 "#586e75")
@@ -1337,45 +1337,47 @@ Subexpression references can be used (\1, \2, etc)."
 ;;                                        (green "#859900"))
 ;;                                    (setq ansi-term-color-vector
 ;;                                          (vconcat `(unspecified ,base02 ,red ,green ,yellow ,blue
-;;                                                                 ,magenta ,cyan ,base2))))))
+;;                                                                 ,magenta ,cyan ,base2))))
+                                                                                                 ))
 
-;;     (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
-;;       (if (memq (process-status proc) '(signal exit))
-;;           (let ((buffer (process-buffer proc)))
-;;             ad-do-it
-;;             (kill-buffer buffer))
-;;         ad-do-it))
-;;     (ad-activate 'term-sentinel)
+    (defadvice term-sentinel (around my-advice-term-sentinel (proc msg))
+      (if (memq (process-status proc) '(signal exit))
+          (let ((buffer (process-buffer proc)))
+            ad-do-it
+            (kill-buffer buffer))
+        ad-do-it))
+    (ad-activate 'term-sentinel)
 
-;;     (defvar my-term-shell "/usr/local/bin/zsh")
-;;     (defadvice ansi-term (before force-bash)
-;;       (interactive (list my-term-shell)))
-;;     (ad-activate 'ansi-term)
+    (defvar my-term-shell "/usr/local/bin/zsh")
+    (defadvice ansi-term (before force-bash)
+      (interactive (list my-term-shell)))
+    (ad-activate 'ansi-term)
 
-;;     (setenv "PATH" (shell-command-to-string "echo $PATH"))
+    (setenv "PATH" (shell-command-to-string "echo $PATH"))
 
-;;     (defun visit-ansi-term ()
-;;       (interactive)
-;;       "Creates an ansi-term and switches to it. If a buffer with name already exists, we simply switch to it."
-;;       (let ((buffer-of-name (get-buffer (concat "*ansi-term-" (wg-name (wg-current-workgroup)))))
-;;             ;;        (default-directory "/home/www")
-;;             (term-cmd "/bin/zsh")
-;;             )
-;;         (cond ((bufferp buffer-of-name) ;If the buffer exists, switch to it (assume it is a shell)
-;;                (switch-to-buffer buffer-of-name))
-;;               ( t
-;;                 (progn
-;;                   (ansi-term term-cmd)
-;;                                         ;(process-send-string (get-buffer-process new-buff-name) (concat "cd " localdir "\n"))
-;;                   (rename-buffer  (concat "*ansi-term-" (wg-name (wg-current-workgroup)))))))))
+    (defun visit-ansi-term ()
+      (interactive)
+      "Creates an ansi-term and switches to it. If a buffer with name already exists, we simply switch to it."
+      (let ((buffer-of-name (get-buffer (concat "*ansi-term-" (wg-name (wg-current-workgroup)))))
+            ;;        (default-directory "/home/www")
+            (term-cmd "/bin/zsh")
+            )
+        (cond ((bufferp buffer-of-name) ;If the buffer exists, switch to it (assume it is a shell)
+               (switch-to-buffer buffer-of-name))
+              ( t
+                (progn
+                  (ansi-term term-cmd)
+                                        ;(process-send-string (get-buffer-process new-buff-name) (concat "cd " localdir "\n"))
+                  (rename-buffer  (concat "*ansi-term-" (wg-name (wg-current-workgroup)))))))))
 
-;;     ))
+     ))
 
-;; (use-package ansi-color
-;;   :config
-;;   (progn
-;;     (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
-;;     (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)))
+(use-package ansi-color
+  :config
+  (progn
+    (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+    (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+        ))
 
 
 ;;;_ , ascii
@@ -5360,14 +5362,32 @@ This one changes the cursor color on each blink. Define colors in `blink-cursor-
 ;; ispell isn't available on OS X, but aspell is via Homebrew
 ;;(setq-default ispell-program-name "aspell")
 
+
+
 ;; Use Solarized-dark on OS X
 ;; we load the theme after init because we might have changed some
 ;; variables in customize
 
-;; (add-hook 'after-init-hook
-;;           (lambda ()
+(add-hook 'after-init-hook
+          (lambda ()
+            (load-theme 'deeper-blue) t)
 ;;             (load-theme 'solarized-dark t)
-;;             (load-theme 'solarized-light t t)))
+;;             (load-theme 'solarized-light t t)
+)
+
+
+(if running-alternate-emacs
+      (progn
+        (add-hook 'after-init-hook
+          (lambda ()
+            (load-theme 'solarized-dark t)
+            (load-theme 'solarized-light t t))))
+    (add-hook 'after-init-hook
+          (lambda ()
+            (load-theme 'deeper-blue) t)))
+
+
+
 
 ;; Even though we may have set the Mac OS X Terminal's Alt key as the
 ;; emacs Meta key, we want to be able to insert a '#' using Alt-3 in
@@ -5397,7 +5417,7 @@ This one changes the cursor color on each blink. Define colors in `blink-cursor-
 (defalias 'td 'toggle-debug-on-error)
 
 ;; Allow "y or n" instead of "yes or no"
-(fset 'yes-or-no-p 'y-or-n-p)
+;; (fset 'yes-or-no-p 'y-or-n-p)
 
 (require 'switch-window)
 
