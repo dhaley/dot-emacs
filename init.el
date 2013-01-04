8281308b21f1655b13731be0f1120c05d884520c
Damon Haley <damon.haley@colorado.edu>
2013-01-04 11:01:01 -0700

    Fri Jan  4 11:01:01 MST 2013

--- init.el
+++ init.el
@@ -2331,8 +2331,8 @@ $ find . -type f \\( -name '*.php' -o -name '*.module' -o -name '*.install' -o -
                                                 :port 6667))
                        :secret))))
 
-    ;; (add-hook 'after-init-hook 'im)
-    ;; (add-hook 'after-init-hook 'irc)
+    (add-hook 'after-init-hook 'im)
+    (add-hook 'after-init-hook 'irc)
     )
 
   :config
@@ -2649,35 +2649,35 @@ at the beginning of line, if already there."
 
 
     ;;This makes Eshell’s ‘ls’ file names RET-able. Yay!
-    ;;   (eval-after-load "em-ls"
-    ;;     '(progn
-    ;;        (defun ted-eshell-ls-find-file-at-point (point)
-    ;;          "RET on Eshell's `ls' output to open files."
-    ;;          (interactive "d")
-    ;;          (find-file (buffer-substring-no-properties
-    ;;                      (previous-single-property-change point 'help-echo)
-    ;;                      (next-single-property-change point 'help-echo))))
+      (eval-after-load "em-ls"
+        '(progn
+           (defun ted-eshell-ls-find-file-at-point (point)
+             "RET on Eshell's `ls' output to open files."
+             (interactive "d")
+             (find-file (buffer-substring-no-properties
+                         (previous-single-property-change point 'help-echo)
+                         (next-single-property-change point 'help-echo))))
 
-    ;;        (defun pat-eshell-ls-find-file-at-mouse-click (event)
-    ;;          "Middle click on Eshell's `ls' output to open files.
-    ;;    From Patrick Anderson via the wiki."
-    ;;          (interactive "e")
-    ;;          (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
+           (defun pat-eshell-ls-find-file-at-mouse-click (event)
+             "Middle click on Eshell's `ls' output to open files.
+       From Patrick Anderson via the wiki."
+             (interactive "e")
+             (ted-eshell-ls-find-file-at-point (posn-point (event-end event))))
 
-    ;;        (let ((map (make-sparse-keymap)))
-    ;;          (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
-    ;;          (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
-    ;;          (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
-    ;;          (defvar ted-eshell-ls-keymap map))
+           (let ((map (make-sparse-keymap)))
+             (define-key map (kbd "RET")      'ted-eshell-ls-find-file-at-point)
+             (define-key map (kbd "<return>") 'ted-eshell-ls-find-file-at-point)
+             (define-key map (kbd "<mouse-2>") 'pat-eshell-ls-find-file-at-mouse-click)
+             (defvar ted-eshell-ls-keymap map))
 
-    ;;        (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
-    ;;          "Eshell's `ls' now lets you click or RET on file names to open them."
-    ;;          (add-text-properties 0 (length ad-return-value)
-    ;;                               (list 'help-echo "RET, mouse-2: visit this file"
-    ;;                                     'mouse-face 'highlight
-    ;;                                     'keymap ted-eshell-ls-keymap)
-    ;;                               ad-return-value)
-    ;;          ad-return-value)))
+           (defadvice eshell-ls-decorated-name (after ted-electrify-ls activate)
+             "Eshell's `ls' now lets you click or RET on file names to open them."
+             (add-text-properties 0 (length ad-return-value)
+                                  (list 'help-echo "RET, mouse-2: visit this file"
+                                        'mouse-face 'highlight
+                                        'keymap ted-eshell-ls-keymap)
+                                  ad-return-value)
+             ad-return-value)))
 
     ;;   (defun ted-eshell-ls-find-file ()
     ;;           (interactive)
@@ -2729,13 +2729,11 @@ at the beginning of line, if already there."
 
     ;; ;; aliases
 
-    ;; (defalias 'open 'find-file)
-    ;; (defalias 'openo 'find-file-other-window)
+    (defalias 'open 'find-file)
+    (defalias 'openo 'find-file-other-window)
 
-    ;; (defun eshell/emacs (file)
-    ;;           (find-file file))
-
-
+    (defun eshell/emacs (file)
+              (find-file file))
 
 
     ;;   (require 'em-smart)
@@ -2778,7 +2776,10 @@ at the beginning of line, if already there."
     ;;   buffer)
 
 
-
+    (add-hook 'eshell-mode-hook
+              '(lambda ()
+                 (local-set-key "\C-c\C-q" 'eshell-kill-process)
+                 (local-set-key "\C-c\C-k" 'compile)))
     ))
 
 ;; eshell
