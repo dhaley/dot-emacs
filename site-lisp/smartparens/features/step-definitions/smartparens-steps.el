e04986bd368de0d1b52faf4ef9cffe8f3e354c68
Damon Haley <damon.haley@colorado.edu>
2013-02-25 10:04:26 -0700

    updated libs

---
+++ site-lisp/smartparens/features/step-definitions/smartparens-steps.el
@@ -0,0 +1,38 @@
+;; This file contains your project specific step definitions. All
+;; files in this directory whose names end with "-steps.el" will be
+;; loaded automatically by Ecukes.
+
+(When "^I exchange point and mark$"
+      (lambda ()
+	(exchange-point-and-mark)))
+
+(Then "^last operation should be \"\\([^\"]+\\)\"$"
+      (lambda (value)
+	(let ((message "sp-last-action should be %s, but is %s")
+	      (last-op (symbol-name sp-last-operation)))
+	  (assert (equal last-op value) nil message value last-op))))
+
+(Then "^last wrapped region should be non-nil$"
+      (lambda ()
+	(assert (not (null sp-last-wrapped-region)) nil "sp-last-wrapped-region should be non-nil, but is nil")))
+
+(Given "^I turn on smartparens globally$"
+       (lambda ()
+         (smartparens-global-mode 1)))
+
+(Given "^I turn on smartparens$"
+       (lambda ()
+         (smartparens-mode 1)))
+
+(Given "^I turn off smartparens$"
+       (lambda ()
+         (smartparens-mode -1)))
+
+(Given "^I add a local pair \"\\([^\"]+\\)\" on \"\\([^\"]+\\)\" ?\\(.*\\)$"
+       (lambda (pair modes-1 modifier)
+         (let ((args (split-string pair "/"))
+               (modes (mapcar #'intern (split-string modes-1 ","))))
+           (cond
+            ((equal "enabled only in string" modifier)
+             (setq args (append args '(:when (sp-in-string-p))))))
+           (apply #'sp-local-pair modes args))))
