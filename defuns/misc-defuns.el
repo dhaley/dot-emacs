429bf8037e1830e6169ebc76d4ca07b665125fd6
Damon Haley <damon.haley@colorado.edu>
2013-08-16 10:24:22 -0600

    added increment/ decrement functions

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -380,3 +380,110 @@ sorted in a more useful way."
     (delete-forward-char 1)
     (just-one-space)))
 
+
+
+; -----------------------------------------------------------------------------
+;; increment/decrement region
+;; -----------------------------------------------------------------------------
+(defun increment-region (&optional beg end arg)
+"Increment all decimal numbers in region between `beg' and
+`end' by `arg'. If no prefix arg is given, increment by 1. If the
+mark is not active, try to build a region using
+`symbol-at-point'."
+(interactive "r\np")
+(or arg (setq arg 1))
+(unless (and mark-active transient-mark-mode)
+(let ((bounds (bounds-of-thing-at-point 'symbol)))
+(if bounds (setq beg (car bounds) end (cdr bounds)))))
+(if (< end beg)
+(let ((tmp end))
+(setq beg end end tmp)))
+(save-excursion
+(goto-char beg)
+(while (re-search-forward "-?[0-9]+" end t)
+(replace-match (number-to-string (+ arg (string-to-number (match-string 0)))))))
+(setq deactivate-mark nil))
+
+(defun decrement-region (&optional beg end arg)
+"Decrement all decimal numbers in region between `beg' and
+`end' by `arg'. If no prefix arg is given, increment by 1. If the
+mark is not active, try to build a region using
+`symbol-at-point'."
+(interactive "r\np")
+(or arg (setq arg 1))
+(unless (and mark-active transient-mark-mode)
+(let ((bounds (bounds-of-thing-at-point 'symbol)))
+(if bounds (setq beg (car bounds) end (cdr bounds)))))
+(increment-region beg end (- arg)))
+
+;; (global-set-key (kbd "<c-s-up>") 'increment-region)
+;; (global-set-key (kbd "<c-s-down>") 'decrement-region)
+
+
+
+(require 'thingatpt)
+
+(defun thing-at-point-goto-end-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip over optional sign
+    (when (looking-at "[+-]")
+      (forward-char 1))
+    ;; Skip over digits
+    (skip-chars-forward "[[:digit:]]")
+    ;; Check for at least one digit
+    (unless (looking-back "[[:digit:]]")
+      (error "No integer here"))))
+(put 'integer 'beginning-op 'thing-at-point-goto-end-of-integer)
+
+(defun thing-at-point-goto-beginning-of-integer ()
+  "Go to end of integer at point."
+  (let ((inhibit-changing-match-data t))
+    ;; Skip backward over digits
+    (skip-chars-backward "[[:digit:]]")
+    ;; Check for digits and optional sign
+    (unless (looking-at "[+-]?[[:digit:]]")
+      (error "No integer here"))
+    ;; Skip backward over optional sign
+    (when (looking-back "[+-]")
+        (backward-char 1))))
+(put 'integer 'beginning-op 'thing-at-point-goto-beginning-of-integer)
+
+(defun thing-at-point-bounds-of-integer-at-point ()
+  "Get boundaries of integer at point."
+  (save-excursion
+    (let (beg end)
+      (thing-at-point-goto-beginning-of-integer)
+      (setq beg (point))
+      (thing-at-point-goto-end-of-integer)
+      (setq end (point))
+      (cons beg end))))
+(put 'integer 'bounds-of-thing-at-point 'thing-at-point-bounds-of-integer-at-point)
+
+(defun thing-at-point-integer-at-point ()
+  "Get integer at point."
+  (let ((bounds (bounds-of-thing-at-point 'integer)))
+    (string-to-number (buffer-substring (car bounds) (cdr bounds)))))
+(put 'integer 'thing-at-point 'thing-at-point-integer-at-point)
+
+(defun increment-integer-at-point (&optional inc)
+  "Increment integer at point by one.
+
+With numeric prefix arg INC, increment the integer by INC amount."
+  (interactive "p")
+  (let ((inc (or inc 1))
+        (n (thing-at-point 'integer))
+        (bounds (bounds-of-thing-at-point 'integer)))
+    (delete-region (car bounds) (cdr bounds))
+    (insert (int-to-string (+ n inc)))))
+
+(defun decrement-integer-at-point (&optional dec)
+  "Decrement integer at point by one.
+
+With numeric prefix arg DEC, decrement the integer by DEC amount."
+  (interactive "p")
+  (increment-integer-at-point (- (or dec 1))))
+
+
+(global-set-key (kbd "C-c +") 'increment-integer-at-point)
+(global-set-key (kbd "C-c -") 'decrement-integer-at-point)
