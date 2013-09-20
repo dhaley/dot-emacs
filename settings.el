69f16c13c1bbbe66648d616f72f58f528fc323bc
Damon Haley <damon.haley@colorado.edu>
2013-09-19 21:08:42 -0600

    diff context

--- settings.el
+++ settings.el
@@ -377,7 +377,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(diff-context ((((background dark)) (:foreground "White")) (t (:foreground "Black"))))
  '(diff-file-header ((((background dark)) (:foreground "Cyan" :background "Black")) (t (:foreground "Red" :background "White"))))
  '(diff-header ((((background dark)) (:foreground "Cyan")) (t (:foreground "Red"))))
  '(diff-hunk-header ((t (:background "grey90" :foreground "black"))))
@@ -387,6 +386,7 @@
  '(diff-added ((((background dark)) (:foreground "#93a1a1")) (t (:foreground "#859900"))))
  '(diff-added2 ((t (:foreground "#2aa198"))) t)
  '(diff-changed ((((background dark)) (:foreground "#B58900")) (t (:foreground "#268bd2"))))
+ '(diff-context ((((background dark)) (:foreground "#93a1a1")) (t (:foreground "#586e75"))))
  '(diff-refine-change ((t (:inherit diff-refine-change :background nil))))
  '(diff-removed ((t (:foreground "firebrick"))))
  '(diff-removed2 ((t (:foreground "Orange"))) t)
