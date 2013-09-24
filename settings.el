e00ba58a4233f91fde8254b6b9419dbebe9a0475
Damon Haley <damon.haley@colorado.edu>
2013-09-24 10:29:45 -0600

    updated projectile to use projectile-switch-project-action 'projectile-dired

--- settings.el
+++ settings.el
@@ -265,6 +265,7 @@
  '(projectile-globally-ignored-directories (quote (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" "sites/default/files" "modules" "themes" ".shelly" "FirePHPCore" "sites/all" "libraries")))
  '(projectile-globally-ignored-files (quote ("TAGS" ".gitignore" ".gitmodules" ".htaccess" "authorize.php" "cron.php" "install.php" "robots.txt" "update.php" "xmlrpc.php" "web.config")))
  '(projectile-project-root-files (quote ("includes/bootstrap.inc" ".projectile" ".git" ".hg" ".fslckout" ".bzr" "_darcs" "rebar.config" "project.clj" "pom.xml" "build.sbt" "Gemfile" "Makefile")))
+ '(projectile-switch-project-action (quote projectile-dired))
  '(projectile-switch-project-hook (quote (detect-drupal)))
  '(projectile-tags-command "drush gtags")
  '(ps-font-size (quote (8 . 10)))
