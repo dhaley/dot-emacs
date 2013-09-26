dc12bee11bef5dab1000debfd022dea42170d7a1
Damon Haley <damon.haley@colorado.edu>
2013-09-26 12:40:32 -0600

    added slime back

--- init.el
+++ init.el
@@ -4866,6 +4866,87 @@ and view local index.html url"
 (use-package sh-toggle
   :bind ("C-. C-z" . shell-toggle))
 
+;;;_ , slime
+
+(use-package slime
+  :commands (sbcl slime)
+  :init
+  (add-hook
+   'slime-load-hook
+   #'(lambda ()
+       (slime-setup
+        '(slime-asdf
+          slime-autodoc
+          slime-banner
+          slime-c-p-c
+          slime-editing-commands
+          slime-fancy-inspector
+          slime-fancy
+          slime-fuzzy
+          slime-highlight-edits
+          slime-parse
+          slime-presentation-streams
+          slime-presentations
+          slime-references
+          slime-repl
+          slime-sbcl-exts
+          slime-package-fu
+          slime-fontifying-fu
+          slime-mdot-fu
+          slime-scratch
+          slime-tramp
+          ;; slime-enclosing-context
+          ;; slime-typeout-frame
+          slime-xref-browser))
+
+       (define-key slime-repl-mode-map [(control return)] 'other-window)
+
+       (define-key slime-mode-map [return] 'paredit-newline)
+       (define-key slime-mode-map [(control ?h) ?F] 'info-lookup-symbol)))
+
+  :config
+  (progn
+    (eval-when-compile
+      (defvar slime-repl-mode-map))
+
+    (setq slime-net-coding-system 'utf-8-unix)
+
+    (setq slime-lisp-implementations
+          '((sbcl
+             ("sbcl" "--core"
+              "/Users/daha1836/Library/Lisp/sbcl.core-with-slime-X86-64")
+             :init
+             (lambda (port-file _)
+               (format "(swank:start-server %S)\n" port-file)))
+            (ecl ("ecl" "-load" "/Users/daha1836/Library/Lisp/init.lisp"))
+            (clisp ("clisp" "-i" "/Users/daha1836/Library/Lisp/lwinit.lisp"))))
+
+    (setq slime-default-lisp 'sbcl)
+    (setq slime-complete-symbol*-fancy t)
+    (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+
+    (defun sbcl (&optional arg)
+      (interactive "P")
+      (let ((slime-default-lisp (if arg 'sbcl64 'sbcl))
+            (current-prefix-arg nil))
+        (slime)))
+    (defun clisp () (interactive) (let ((slime-default-lisp 'clisp)) (slime)))
+    (defun ecl () (interactive) (let ((slime-default-lisp 'ecl)) (slime)))
+
+    (defun start-slime ()
+      (interactive)
+      (unless (slime-connected-p)
+        (save-excursion (slime))))
+
+    (add-hook 'slime-mode-hook 'start-slime)
+    (add-hook 'slime-load-hook #'(lambda () (require 'slime-fancy)))
+    (add-hook 'inferior-lisp-mode-hook #'(lambda () (inferior-slime-mode t)))
+
+    (use-package hyperspec
+      :init
+      (setq common-lisp-hyperspec-root
+            (expand-file-name "~/Library/Lisp/HyperSpec/")))))
+
 ;;;_ , smart-compile
 
 (defun show-compilation ()
