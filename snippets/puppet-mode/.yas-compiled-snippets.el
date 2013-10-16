9a6a96da66a9de12145a2815adc359986c334489
Damon Haley <damon.haley@colorado.edu>
2013-10-16 11:45:21 -0600

    don't want compiled snippets in repo.

--- snippets/puppet-mode/.yas-compiled-snippets.el
+++
@@ -1,8 +0,0 @@
-;;; Compiled snippets and support files for `puppet-mode'
-;;; Snippet definitions:
-;;;
-(yas-define-snippets 'puppet-mode
-                     '(("dir" "file { \"/home/$1\":\n  owner   => $1,\n  group   => $1,\n  mode    => 0700,\n  type    => directory,\n  ensure  => directory;\n}" "dir" nil nil nil nil nil nil)))
-
-
-;;; Do not edit! File generated at Wed Sep  4 12:29:35 2013
