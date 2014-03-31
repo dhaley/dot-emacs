e3a41577b82b98638bc1592153ddd0a931919436
Damon Haley <damon.haley@colorado.edu>
2014-03-31 13:59:10 -0600

    https://bitbucket.org/Fuco/.emacs.d/commits/6cad2a240aa849eb3ba9436fe2f342e7ad7b7ad7

--- init.el
+++ init.el
@@ -3850,7 +3850,43 @@ Keys are in kbd format."
       (buffer-projectile #'next-buffer))
 
     (global-set-key (kbd "C-c p <left>") 'projectile-switch-to-prev-buffer)
-    (global-set-key (kbd "C-c p <right>") 'projectile-switch-to-next-buffer)))
+    (global-set-key (kbd "C-c p <right>") 'projectile-switch-to-next-buffer))
+  :config
+  (progn
+    ;; https://bitbucket.org/Fuco/.emacs.d/commits/6cad2a240aa849eb3ba9436fe2f342e7ad7b7da7
+    (defun projectile-project-root ()
+      "Retrieves the root directory of a project if available.
+The current directory is assumed to be the project's root otherwise."
+      (let ((project-root
+             (or (->> projectile-project-root-files
+                   (--map (locate-dominating-file (file-truename default-directory) it))
+                   (-remove #'null)
+                   (--max-by (> (s-count-matches "/" it) (s-count-matches "/" other))) ;;; return the closest "parent dir" for this (possible) subproject
+                   (projectile-file-truename))
+                 (if projectile-require-project-root
+                     (error "You're not in a project")
+                   default-directory))))
+        project-root))
+
+    (defun projectile-get-ext-command ()
+      "Determine which external command to invoke based on the project's VCS."
+      (concat
+       "find -L . -not \\( \\( "
+       (mapconcat (lambda (x)
+                    (concat "-path \"*/" x "/*\"")) projectile-globally-ignored-directories " -or ")
+       (let ((proj-ig-dirs (projectile-project-ignored-directories)))
+         (if (not proj-ig-dirs) ""
+           (concat
+            " -or "
+            (mapconcat (lambda (x)
+                         (concat "-path \"" x "\""))
+                       (let ((project-root (projectile-project-root)))
+                         (--map (concat "./" (file-relative-name it project-root)) proj-ig-dirs)) " -or "))))
+       " \\) -prune \\)"
+       " -not "
+       (mapconcat (lambda (x)
+                    (concat "-path \"*/" x "\"")) projectile-globally-ignored-directories " -not ")
+       " -type f -print0"))))
 
 ;;;_ , popup-ruler
 
