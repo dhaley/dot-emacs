ba0e1dab99fa0f1c1d6a85d38afe40581951d117
Damon Haley <damon.haley@colorado.edu>
2014-05-22 15:03:51 -0600

    faces.

--- org-settings.el
+++ org-settings.el
@@ -182,4 +182,4 @@ SCHEDULED: %t
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button)))) t))
