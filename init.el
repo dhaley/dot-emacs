6b718c75bf0aa22d2f6b696a036f38d209250b9c
Haley <damon.haley@nrel.gov>
2015-05-13 16:24:32 -0600

    more cool ace-window stuff.

--- init.el
+++ init.el
@@ -1095,8 +1095,46 @@ Keys are in kbd format."
 (use-package ace-window
   :load-path "site-lisp/ace-window"
   :bind* "<C-return>"
+  :init
+  (defun joe-scroll-other-window()
+    (interactive)
+    (scroll-other-window 1))
+
+  (defun joe-scroll-other-window-down ()
+    (interactive)
+    (scroll-other-window-down 1))
   :config
-  (setq aw-keys (quote (97 111 101 117 105 100 104 116 110)))
+  (setq aw-keys (quote (97 111 101 117 105 100 104 116 110))
+        aw-dispatch-always t
+        aw-dispatch-alist
+        '((?x aw-delete-window     "Ace - Delete Window")
+          (?c aw-swap-window       "Ace - Swap Window")
+          (?n aw-flip-window)
+          (?v aw-split-window-vert "Ace - Split Vert Window")
+          (?h aw-split-window-horz "Ace - Split Horz Window")
+          (?m delete-other-windows "Ace - Maximize Window")
+          (?g delete-other-windows)
+          (?b balance-windows)
+          (?u winner-undo)
+          (?r winner-redo)))
+  (when (package-installed-p 'hydra)
+    (defhydra hydra-window-size (:color red)
+      "Windows size"
+      ("a" shrink-window-horizontally "shrink horizontal")
+      ("." shrink-window "shrink vertical")
+      ("j" enlarge-window "enlarge vertical")
+      ("i" enlarge-window-horizontally "enlarge horizontal"))
+    (defhydra hydra-window-frame (:color red)
+      "Frame"
+      ("f" make-frame "new frame")
+      ("x" delete-frame "delete frame"))
+    (defhydra hydra-window-scroll (:color red)
+      "Scroll other window"
+      ("<SPC>" joe-scroll-other-window "scroll")
+      ("<backspace>" joe-scroll-other-window-down "scroll down"))
+    (add-to-list 'aw-dispatch-alist '(?w hydra-window-size/body) t)
+    (add-to-list 'aw-dispatch-alist '(32 hydra-window-scroll/body) t)
+    (add-to-list 'aw-dispatch-alist '(?\; hydra-window-frame/body) t))
   (set-face-attribute 'aw-leading-char-face nil :foreground "deep sky blue" :weight 'bold :height 3.0)
   (set-face-attribute 'aw-mode-line-face nil :inherit 'mode-line-buffer-id :foreground "lawn green"))
 
