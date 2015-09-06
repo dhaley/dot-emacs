2425c0e684e35d7680612539d7fffe33e8be1dff
Damon Haley <damon.haley@colorado.edu>
2015-09-06 13:34:18 -0600

    various changes.

--- init.el
+++ init.el
@@ -441,13 +441,19 @@ Inspired by Erik Naggum's `recursive-edit-with-single-window'."
    ((eq display-name 'retina-imac)
     (if running-alternate-emacs
         "-*-Myriad Pro-normal-normal-normal-*-20-*-*-*-p-0-iso10646-1"
-      "-*-Source Code Pro-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
+      ;; "-*-Source Code Pro-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"
+      "-*-Hack-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"
+      ))
    ((string= system-name "ubuntu")
-    "-*-Source Code Pro-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1")
+    ;; "-*-Source Code Pro-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1"
+    "-*-Hack-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"
+    )
    (t
     (if running-alternate-emacs
         "-*-Myriad Pro-normal-normal-normal-*-17-*-*-*-p-0-iso10646-1"
-      "-*-Source Code Pro-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"))))
+      ;; "-*-Source Code Pro-normal-normal-normal-*-15-*-*-*-m-0-iso10646-1"
+      "-*-Hack-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"
+      ))))
 
 (let ((frame-alist
        (list (cons 'top    emacs-min-top)
@@ -681,6 +687,34 @@ Inspired by Erik Naggum's `recursive-edit-with-single-window'."
 ;; dkh (2015-03-25): Move all of the above into use-package declarations    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; Delayed configuration
+
+(use-package dot-org
+  :load-path ("override/org-mode/contrib/lisp"
+              "override/org-mode/lisp")
+  :commands my-org-startup
+  :bind (("M-C"   . jump-to-org-agenda)
+         ("M-m"   . org-smart-capture)
+         ("M-M"   . org-inline-note)
+         ("C-c a" . org-agenda)
+         ("C-c S" . org-store-link)
+         ("C-c l" . org-insert-link)
+         ("C-. n" . org-velocity-read))
+  :defer 30
+  :config
+  (when (not running-alternate-emacs)
+    (run-with-idle-timer 300 t 'jump-to-org-agenda)
+    (my-org-startup)))
+
+(use-package dot-gnus
+  :load-path "override/gnus"
+  :bind (("M-G"   . switch-to-gnus)
+         ("C-x m" . compose-mail))
+  :init
+  (progn
+    (setq gnus-init-file (expand-file-name "dot-gnus" user-emacs-directory)
+          gnus-home-directory "~/Messages/Gnus/")))
+
 (defun define-keys (mode-map keybindings)
   "Takes a mode map, and a list of (key function-designator)
 lists.  The functions are bound to the keys in the given mode-map.
@@ -697,301 +731,9 @@ Keys are in kbd format."
   :bind ("M-T" . tags-search))
 
 (use-package ggtags
-  :disabled t
   :load-path "site-lisp/ggtags"
-  :disabled
   :commands ggtags-mode
   :diminish ggtags-mode)
-
-(use-package cc-mode
-  :load-path "override/cc-mode"
-  :mode (("\\.h\\(h?\\|xx\\|pp\\)\\'" . c++-mode)
-         ("\\.m\\'"                   . c-mode)
-         ("\\.mm\\'"                  . c++-mode))
-  :preface
-  (defun my-paste-as-check ()
-    (interactive)
-    (save-excursion
-      (insert "/*\n")
-      (let ((beg (point)) end)
-        (yank)
-        (setq end (point-marker))
-        (goto-char beg)
-        (while (< (point) end)
-          (forward-char 2)
-          (insert "CHECK: ")
-          (forward-line 1)))
-      (insert "*/\n")))
-
-  ;; (defun my-c-indent-or-complete ()
-  ;;   (interactive)
-  ;;   (let ((class (syntax-class (syntax-after (1- (point))))))
-  ;;     (if (or (bolp) (and (/= 2 class)
-  ;;                         (/= 3 class)))
-  ;;         (call-interactively 'indent-according-to-mode)
-  ;;       (call-interactively 'auto-complete))))
-
-  (defvar printf-index 0)
-
-  (defun insert-counting-printf (arg)
-    (interactive "P")
-    (if arg
-        (setq printf-index 0))
-    (if t
-        (insert (format "std::cerr << \"step %d..\" << std::endl;\n"
-                        (setq printf-index (1+ printf-index))))
-      (insert (format "printf(\"step %d..\\n\");\n"
-                      (setq printf-index (1+ printf-index)))))
-    (forward-line -1)
-    (indent-according-to-mode)
-    (forward-line))
-
-  (defun my-c-mode-common-hook ()
-    (abbrev-mode 1)
-    ;; (ggtags-mode 1)
-    (eldoc-mode 1)
-    (hs-minor-mode 1)
-    (hide-ifdef-mode 1)
-    (whitespace-mode 1)
-    (which-function-mode 1)
-    (company-mode 1)
-    (bug-reference-prog-mode 1)
-
-    (diminish 'hs-minor-mode)
-    (diminish 'hide-ifdef-mode)
-
-    ;; (bind-key "C-c p" 'insert-counting-printf c-mode-base-map)
-
-    ;;(doxymacs-mode 1)
-    ;;(doxymacs-font-lock)
-
-    (bind-key "<return>" 'newline-and-indent c-mode-base-map)
-
-    (unbind-key "M-j" c-mode-base-map)
-    (bind-key "C-c C-i" 'c-includes-current-file c-mode-base-map)
-    (bind-key "C-c C-y" 'my-paste-as-check c-mode-base-map)
-
-    (set (make-local-variable 'parens-require-spaces) nil)
-    (setq indicate-empty-lines t)
-    (setq fill-column 72)
-
-    (bind-key "M-q" 'c-fill-paragraph c-mode-base-map)
-
-    (let ((bufname (buffer-file-name)))
-      (when bufname
-        (cond
-         ((string-match "/ledger/" bufname)
-          (c-set-style "ledger"))
-         ((string-match "/ansi/" bufname)
-          (c-set-style "ti")
-          (substitute-key-definition 'fill-paragraph 'ti-refill-comment
-                                     c-mode-base-map global-map)
-          (bind-key "M-q" 'ti-refill-comment c-mode-base-map))
-         ((string-match "/edg/" bufname)
-          (c-set-style "edg"))
-         (t
-          (c-set-style "clang")))))
-
-    (font-lock-add-keywords 'c++-mode '(("\\<\\(assert\\|DEBUG\\)("
-                                         1 font-lock-warning-face t))))
-
-  :config
-  (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
-
-  (setq c-syntactic-indentation nil)
-
-  (bind-key "#" 'self-insert-command c-mode-base-map)
-  (bind-key "{" 'self-insert-command c-mode-base-map)
-  (bind-key "}" 'self-insert-command c-mode-base-map)
-  (bind-key "/" 'self-insert-command c-mode-base-map)
-  (bind-key "*" 'self-insert-command c-mode-base-map)
-  (bind-key ";" 'self-insert-command c-mode-base-map)
-  (bind-key "," 'self-insert-command c-mode-base-map)
-  (bind-key ":" 'self-insert-command c-mode-base-map)
-  (bind-key "(" 'self-insert-command c-mode-base-map)
-  (bind-key ")" 'self-insert-command c-mode-base-map)
-  (bind-key "<" 'self-insert-command c++-mode-map)
-  (bind-key ">" 'self-insert-command c++-mode-map)
-
-  (add-to-list 'c-style-alist
-               '("ti"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 3)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . +)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . c-lineup-arglist-intro-after-paren)
-                     (arglist-close . c-lineup-arglist)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (add-to-list 'c-style-alist
-               '("edg"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . +)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (add-to-list 'c-style-alist
-               '("ledger"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . 0)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (add-to-list 'c-style-alist
-               '("clang"
-                 (indent-tabs-mode . nil)
-                 (c-basic-offset . 2)
-                 (c-comment-only-line-offset . (0 . 0))
-                 (c-hanging-braces-alist
-                  . ((substatement-open before after)
-                     (arglist-cont-nonempty)))
-                 (c-offsets-alist
-                  . ((statement-block-intro . +)
-                     (knr-argdecl-intro . 5)
-                     (substatement-open . 0)
-                     (substatement-label . 0)
-                     (label . 0)
-                     (case-label . 0)
-                     (statement-case-open . 0)
-                     (statement-cont . +)
-                     (arglist-intro . +)
-                     (arglist-close . +)
-                     (inline-open . 0)
-                     (brace-list-open . 0)
-                     (topmost-intro-cont
-                      . (first c-lineup-topmost-intro-cont
-                               c-lineup-gnu-DEFUN-intro-cont))))
-                 (c-special-indent-hook . c-gnu-impose-minimum)
-                 (c-block-comment-prefix . "")))
-
-  (defun opencl ()
-    (interactive)
-    (find-file "~/src/ansi/opencl.c")
-    (find-file-noselect "~/Contracts/TI/bugslayer/cl_0603/cl_0603.c")
-    (find-file-noselect "~/Contracts/TI/bugslayer")
-    (magit-status "~/src/ansi")
-    (gud-gdb "gdb --fullname ~/Contracts/TI/bin/c60/acpia6x"))
-
-  (defun ti-refill-comment ()
-    (interactive)
-    (let ((here (point)))
-      (goto-char (line-beginning-position))
-      (let ((begin (point)) end
-            (marker ?-) (marker-re "\\(-----\\|\\*\\*\\*\\*\\*\\)")
-            (leader-width 0))
-        (unless (looking-at "[ \t]*/\\*[-* ]")
-          (search-backward "/*")
-          (goto-char (line-beginning-position)))
-        (unless (looking-at "[ \t]*/\\*[-* ]")
-          (error "Not in a comment"))
-        (while (and (looking-at "\\([ \t]*\\)/\\* ")
-                    (setq leader-width (length (match-string 1)))
-                    (not (looking-at (concat "[ \t]*/\\*" marker-re))))
-          (forward-line -1)
-          (setq begin (point)))
-        (when (looking-at (concat "[^\n]+?" marker-re "\\*/[ \t]*$"))
-          (setq marker (if (string= (match-string 1) "-----") ?- ?*))
-          (forward-line))
-        (while (and (looking-at "[^\n]+?\\*/[ \t]*$")
-                    (not (looking-at (concat "[^\n]+?" marker-re
-                                             "\\*/[ \t]*$"))))
-          (forward-line))
-        (when (looking-at (concat "[^\n]+?" marker-re "\\*/[ \t]*$"))
-          (forward-line))
-        (setq end (point))
-        (let ((comment (buffer-substring-no-properties begin end)))
-          (with-temp-buffer
-            (insert comment)
-            (goto-char (point-min))
-            (flush-lines (concat "^[ \t]*/\\*" marker-re "[-*]+\\*/[ \t]*$"))
-            (goto-char (point-min))
-            (while (re-search-forward "^[ \t]*/\\* ?" nil t)
-              (goto-char (match-beginning 0))
-              (delete-region (match-beginning 0) (match-end 0)))
-            (goto-char (point-min))
-            (while (re-search-forward "[ \t]*\\*/[ \t]*$" nil t)
-              (goto-char (match-beginning 0))
-              (delete-region (match-beginning 0) (match-end 0)))
-            (goto-char (point-min)) (delete-trailing-whitespace)
-            (goto-char (point-min)) (flush-lines "^$")
-            (set-fill-column (- 80      ; width of the text
-                                6       ; width of "/*  */"
-                                leader-width))
-            (goto-char (point-min)) (fill-paragraph nil)
-            (goto-char (point-min))
-            (while (not (eobp))
-              (insert (make-string leader-width ? ) "/* ")
-              (goto-char (line-end-position))
-              (insert (make-string (- 80 3 (current-column)) ? ) " */")
-              (forward-line))
-            (goto-char (point-min))
-            (insert (make-string leader-width ? )
-                    "/*" (make-string (- 80 4 leader-width) marker) "*/\n")
-            (goto-char (point-max))
-            (insert (make-string leader-width ? )
-                    "/*" (make-string (- 80 4 leader-width) marker) "*/\n")
-            (setq comment (buffer-string)))
-          (goto-char begin)
-          (delete-region begin end)
-          (insert comment)))
-      (goto-char here))))
 
 (use-package abbrev
   :disabled t
@@ -1011,7 +753,7 @@ Keys are in kbd format."
 
 (use-package ace-jump-mode
   :load-path "site-lisp/ace-jump-mode"
-  :bind ("C-c SPC" . ace-jump-mode)
+  :bind ("M-h" . ace-jump-mode)
   :config
   (setq ace-jump-mode-submode-list
         '(ace-jump-char-mode
@@ -2368,8 +2110,7 @@ You can use arrow-keys or WASD.
                 (split-string omitted-files "\n" t)
                 "\\|")
                "\\)")))
-        (funcall dired-omit-regexp-orig))))
-  (setq insert-directory-program (executable-find "gls")))
+        (funcall dired-omit-regexp-orig)))))
 
 (use-package dired-toggle
   :load-path "site-lisp/dired-toggle"
@@ -2827,20 +2568,6 @@ You can use arrow-keys or WASD.
   :commands git-wip-mode
   :init (add-hook 'find-file-hook #'(lambda () (git-wip-mode 1))))
 
-(use-package dot-gnus
-  :load-path "override/gnus"
-  :bind (("M-G"   . switch-to-gnus)
-         ("C-x m" . compose-mail))
-  :init
-  (progn
-    (setq gnus-init-file (expand-file-name "dot-gnus" user-emacs-directory)
-          gnus-home-directory "~/Messages/Gnus/"))
-  :config
-  (progn
-    (use-package org-mime
-      :load-path "override/org-mode/contrib/lisp")
-    (setq org-mime-library 'mml)))
-
 (use-package golden-ratio
   :diminish golden-ratio-mode
   :commands golden-ratio-mode
@@ -3035,7 +2762,7 @@ You can use arrow-keys or WASD.
          ("C-x f"   . helm-multi-files)
          ("M-s b"   . helm-occur)
          ("M-s n"   . my-helm-find)
-         ("M-h"     . helm-resume))
+         ("M-H"     . helm-resume))
 
   :preface
   (defun my-helm-find ()
@@ -3755,7 +3482,7 @@ You can use arrow-keys or WASD.
 
       (use-package eldoc
         :diminish eldoc-mode
-        :defer t
+        :commands eldoc-mode
         :config
         (use-package eldoc-extension
           :disabled t
@@ -3765,6 +3492,7 @@ You can use arrow-keys or WASD.
                     #'(lambda () (require 'eldoc-extension)) t))
         (eldoc-add-command 'paredit-backward-delete
                            'paredit-close-round))
+
 
       (use-package cldoc
         :diminish cldoc-mode)
@@ -4155,22 +3883,6 @@ You can use arrow-keys or WASD.
   :defer 5
   :config
   (on-screen-global-mode 1))
-
-(use-package dot-org
-  :load-path "override/org-mode/lisp"
-  :commands my-org-startup
-  :bind (("M-C"   . jump-to-org-agenda)
-         ("M-m"   . org-smart-capture)
-         ("M-M"   . org-inline-note)
-         ("C-c a" . org-agenda)
-         ("C-c S" . org-store-link)
-         ("C-c l" . org-insert-link)
-         ("C-. n" . org-velocity-read))
-  :defer 30
-  :config
-  (when (not running-alternate-emacs)
-    (run-with-idle-timer 300 t 'jump-to-org-agenda)
-    (my-org-startup)))
 
 (use-package osx-trash
   :load-path "site-lisp/osx-trash"
